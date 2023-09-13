49cce06c0b3576e24304a89a4781a4ce525cde07
Nico T <ntorassa@betterfly.com>
2023-09-13 17:00:31 -0300

    Empty structure finished

---
+++ README.md
@@ -0,0 +1,3 @@
+Latest version:
+
+[![](https://jitpack.io/v/Truvideo/truvideo-sdk-android-media.svg)](https://jitpack.io/#Truvideo/truvideo-sdk-android-media)
---
+++ media/src/main/java/com/truvideo/sdk/media/TruvideoSdkMedia.kt
@@ -0,0 +1,16 @@
+@file:Suppress("unused")
+
+package com.truvideo.sdk.media
+
+import androidx.activity.ComponentActivity
+
+object TruvideoSdkMedia {
+    fun init(activity: ComponentActivity) {
+        // TODO: not implemented
+    }
+
+    fun start(): Boolean {
+        // TODO: not implemented
+        return false
+    }
+}
